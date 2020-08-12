b6d8cb3765610ffecc8133b4bf7a2f558dac7afa
robbinl <robbinl@telusplanet.net>
2020-08-12 15:15:40 -0600

    a

--- src/settings.h
+++ src/settings.h
@@ -1,14 +1,11 @@
 #ifndef SETTINGS_H
     #define SETTINGS_H
-    //#define LED_BUILTIN 2   // This is valid for my devkit
     #include "Arduino.h"
     #include <Adafruit_Sensor.h>
     #include <Adafruit_BME280.h>
     #include "SPIFFS.h"
     #include "FS.h"
     #include <TFT_eSPI.h>
-    
-    //#include "WiFi.h"
     #include "AdafruitIO_WiFi.h"
     #include "ezTime.h"
     #include "AdafruitIO_Feed.h"
