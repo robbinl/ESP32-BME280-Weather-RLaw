770d9679e9ffdd811b0c0e7d025bb3772604f3d6
robbinl <robbinl@telusplanet.net>
2020-08-20 17:35:22 -0600

    a

--- src/aux_functions.h
+++ src/aux_functions.h
@@ -1,18 +1,17 @@
-//#include <Arduino.h>
 #include "settings.h"
 #ifndef AUX_FUNCTIONS_H
     #define AUX_FUNCTIONS_H
     
 
-    void wifiStatus(    TFT_eSPI* tft, 
+    void wifiStatus(TFT_eSPI* tft, 
                     AdafruitIO_WiFi* io);
 
-    void indicators(    TFT_eSPI* tft, 
-                        AdafruitIO_WiFi* io);
+    void indicators(TFT_eSPI* tft, 
+                    AdafruitIO_WiFi* io);
 
     const char* wl_status_to_string(wl_status_t status);
 
-    void postsCounter(  TFT_eSPI* tft);
+    void postsCounter(TFT_eSPI* tft);
     
 #endif
 
